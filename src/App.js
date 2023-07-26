e64157a1e3618532fbc2074ce4aff6688be9df58
zinckle <mzinck9@gmail.com>
2023-07-26 18:03:04 -0400

    stuck on async stuff

--- src/App.js
+++ src/App.js
@@ -6,40 +6,36 @@ import EmojiPicker, {
   Emoji,
   PreviewConfig,
 } from "emoji-picker-react";
+import { async } from "react-input-emoji";
 
-export default function App() {
-  var list = [];
-  /*
-  d3.tsv("2021_ranked.tsv", function (data) {
-    let code = data.Hex.slice(3);
-    code = code.substring(0, code.length - 1);
-    data.Hex = code;
-    if (!(data.Category === "Flags" || data.Category === "Component")) {
-      list.push(data);
-    }
-  });*/
+export default async function App() {
+  var date = new Date()
 
-  //console.log(list);
+  var seed = cyrb128(date.getFullYear() + " " + date.getMonth() + " " + date.getDate());
+  var rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
 
-  /*let options = {
-    method: 'GET',
-    headers: { 'x-api-key': 'VIyUjX078yMMyLukW85EdA==FrpvQaHWnlOgCXkj' }
-  }
-  
-  //let hex = "😀".codePointAt(0).toString(16)
+  var listOfPossibleGuesses = [];
+  var listOfPossibleAnswers = [];
 
-  let code = '1F642'
+    // Wait for the data to load using async/await
+    var data = await loadTSVData("2021_ranked.tsv");
 
-  let url = 'https://api.api-ninjas.com/v1/emoji?code=' + code
-  
-  fetch(url,options)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-          console.log(data)
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        }); */
+    data.forEach((item) => {
+      let code = item.Hex.slice(3);
+      code = code.substring(0, code.length - 1);
+      item.Hex = code;
+      listOfPossibleGuesses.push(item);
+      if (!(item.Category === "Flags" || item.Category === "Component")) {
+        listOfPossibleAnswers.push(item);
+      }
+    });
+
+  rand = Math.floor(rand*10000)
+  rand = rand%listOfPossibleAnswers
+
+  let emojiOfTheDay = listOfPossibleAnswers[rand]
+
+  console.log(emojiOfTheDay)
 
   const inputRef = useRef(null);
 
@@ -51,22 +47,20 @@ export default function App() {
 
   const handleClick = () => {
     // 👇 "inputRef.current.value" is input value
-    console.log(selectedEmoji.emoji)
-    addItem(selectedEmoji.emoji)
+    console.log(selectedEmoji.emoji);
+    addItem(selectedEmoji.emoji);
   };
-
 
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       // 👇 Get input value
-      addItem(selectedEmoji.emoji)
-      console.log(selectedEmoji.emoji)
-
+      addItem(selectedEmoji.emoji);
+      console.log(selectedEmoji.emoji);
     }
   };
 
   const onClick = (emojiData, event) => {
-    console.log(selectedEmoji.emoji)
+    console.log(selectedEmoji.emoji);
     setSelectedEmoji(emojiData);
   };
 
@@ -78,6 +72,7 @@ export default function App() {
           emojiVersion="11.0"
           skinTonesDisabled="true"
           suggestedEmojisMode="recent"
+          theme="dark"
         />
       </div>
 
@@ -94,7 +89,13 @@ export default function App() {
         )}
         <br />
         <br />
-        <button class = "button-20" onClick={handleClick} onKeyDown={handleKeyDown}>Submit</button>
+        <button
+          class="button-20"
+          onClick={handleClick}
+          onKeyDown={handleKeyDown}
+        >
+          Submit
+        </button>
       </div>
       <div id="parent" class="border"></div>
     </div>
@@ -102,32 +103,65 @@ export default function App() {
 }
 
 function addItem(selectedEmoji) {
-      
-  // Get type of element from form
-  let type = "div";
-
-  // Get the text/value for the tag
-  // from the form
   let value = selectedEmoji;
 
-  // createElement() is used for
-  // creating a new element
-  type
-      = document.createElement(type);
+  document
+    .getElementById("parent")
+    .appendChild(
+      Object.assign(document.createElement("div"), {
+        innerHTML: value,
+        id: "answer",
+        className: "border",
+      })
+    );
+}
 
-  // Use value as textnode in this example
-  type.appendChild(
-      document.createTextNode(value));
+function cyrb128(str) {
+  let h1 = 1779033703,
+    h2 = 3144134277,
+    h3 = 1013904242,
+    h4 = 2773480762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+  return [
+    (h1 ^ h2 ^ h3 ^ h4) >>> 0,
+    (h2 ^ h1) >>> 0,
+    (h3 ^ h1) >>> 0,
+    (h4 ^ h1) >>> 0,
+  ];
+}
 
-  // Append as child to the parent
-  // tag i.e. ol
-  document.getElementById(
-      "parent").appendChild(
-        Object.assign(
-          document.createElement('div'),
-          { innerHTML: value,
-            id: "answer",
-            className:"border"
-        }
-        ));
+function sfc32(a, b, c, d) {
+    a >>>= 0;
+    b >>>= 0;
+    c >>>= 0;
+    d >>>= 0;
+    var t = (a + b) | 0;
+    a = b ^ (b >>> 9);
+    b = (c + (c << 3)) | 0;
+    c = (c << 21) | (c >>> 11);
+    d = (d + 1) | 0;
+    t = (t + d) | 0;
+    c = (c + t) | 0;
+    return (t >>> 0) / 4294967296;
+  };
+
+  async function loadTSVData(file) {
+    return new Promise(function (resolve, reject) {
+      var test = []
+      d3.tsv(file, function (data) {
+          
+        test.push(data)}
+      );
+      return(test);
+    });
   }
